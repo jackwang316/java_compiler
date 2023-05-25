f01d656625f7cc67b4a1548d725db85e1489b3fc
jackwang316 <jackwang316@yahoo.ca>
2023-05-24 21:45:19 -0700

    "floats working"

---
+++ src/parseTree/nodeTypes/FloatingConstantNode.java
@@ -0,0 +1,36 @@
+package parseTree.nodeTypes;
+
+import parseTree.ParseNode;
+import parseTree.ParseNodeVisitor;
+import tokens.FloatingLiteralToken;
+import tokens.IntegerLiteralToken;
+import tokens.Token;
+
+public class FloatingConstantNode extends ParseNode {
+	public FloatingConstantNode(Token token) {
+		super(token);
+		assert(token instanceof FloatingLiteralToken);
+	}
+	public FloatingConstantNode(ParseNode node) {
+		super(node);
+	}
+
+////////////////////////////////////////////////////////////
+// attributes
+	
+	public double getValue() {
+		return numberToken().getValue();
+	}
+
+	public FloatingLiteralToken numberToken() {
+		return (FloatingLiteralToken)token;
+	}	
+
+///////////////////////////////////////////////////////////
+// accept a visitor
+	
+	public void accept(ParseNodeVisitor visitor) {
+		visitor.visit(this);
+	}
+
+}
---
+++ src/tokens/IntegerLiteralToken.java
@@ -0,0 +1,28 @@
+package tokens;
+
+import inputHandler.Locator;
+
+public class IntegerLiteralToken extends TokenImp {
+	protected int value;
+	
+	protected IntegerLiteralToken(Locator locator, String lexeme) {
+		super(locator, lexeme);
+	}
+	protected void setValue(int value) {
+		this.value = value;
+	}
+	public int getValue() {
+		return value;
+	}
+	
+	public static IntegerLiteralToken make(Locator locator, String lexeme) {
+		IntegerLiteralToken result = new IntegerLiteralToken(locator, lexeme);
+		result.setValue(Integer.parseInt(lexeme));
+		return result;
+	}
+	
+	@Override
+	protected String rawString() {
+		return "number, " + value;
+	}
+}
