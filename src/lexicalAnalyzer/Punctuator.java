326846fd3542e66395679bde967e9eff50d69d3a
jackwang316 <jackwang316@yahoo.ca>
2023-06-04 10:05:23 -0700

    Added brackets to punctuation

--- src/lexicalAnalyzer/Punctuator.java
+++ src/lexicalAnalyzer/Punctuator.java
@@ -17,6 +17,10 @@ public enum Punctuator implements Lextant {
 	TERMINATOR(";"), 
 	OPEN_BRACE("{"),
 	CLOSE_BRACE("}"),
+	OPEN_PARENTHESIS("("),
+	CLOSE_PARENTHESIS(")"),
+	OPEN_BRACKET("["),
+	CLOSE_BRACKET("]"),
 	NULL_PUNCTUATOR("");
 
 	private String lexeme;
